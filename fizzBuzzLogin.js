ebe5ebabdec75b5e8669a03d7b4dae71f3f46fb8
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-06-13 16:40:34 -0500

    lastest update api validation

--- fizzBuzzLogin.js
+++ fizzBuzzLogin.js
@@ -1,5 +1,20 @@
-function fizzBuzzLogin() {
-  console.log("hello");
+function fizzBuzzLogin(req, res) {
+  let object = {
+    user: "Fizz",
+    password: "Buzz"
+  };
+
+  if (req.body.user === object.user && req.body.password === object.password) {
+    res.send({ resposne: "Access Granted" });
+  } else {
+    res.send({ resposne: "Access Denied" });
+  }
+
+  //   if (this.user == user) {
+  //     res.send(user);
+  //   } else {
+  //     res.send("blah");
+  //   }
 }
 
 module.exports = fizzBuzzLogin;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const fizzBuzzLogin = require("./fizzBuzzLogin");
 const fizzBuzzParam = require("./fizzBuzzLogic");
 const app = express();
@@ -6,13 +7,15 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
   res.send("FizzBuzzApp");
   next();
 });
 
-app.get("/login", fizzBuzzLogin);
+app.use("/api/login", fizzBuzzLogin);
 
 app.use("/api/fizzBuzz", fizzBuzzParam);
 
