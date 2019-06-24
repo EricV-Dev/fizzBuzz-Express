3cf4969ccdbc4a087fc5ce9da56fc07a8d42f8b5
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-06-23 21:07:19 -0500

    latest update with bcrypt

--- fizzBuzzLogin.js
+++ fizzBuzzLogin.js
@@ -1,10 +1,15 @@
+let bcrypt = require("bcryptjs");
+
 function fizzBuzzLogin(req, res) {
   let object = {
     user: "Fizz",
-    password: "Buzz"
+    password: "$2a$10$8GWVflBMw2ZaE3YqrqLD0uMjGzUXyx.lGsJDQ6o1tb8xRMekbntyu"
   };
 
-  if (req.body.user === object.user && req.body.password === object.password) {
+  if (
+    req.body.user === object.user &&
+    bcrypt.compareSync(req.body.password, object.password) === true
+  ) {
     res.status(200).send({ response: "Access Granted" });
   } else {
     res.sendStatus(401);
