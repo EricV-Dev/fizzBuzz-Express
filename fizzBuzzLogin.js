d768affb027784ad33d15c5f506786eef8f3f1ab
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-07-20 16:19:21 -0500

    fixed connection

--- fizzBuzzLogin.js
+++ fizzBuzzLogin.js
@@ -1,44 +1,39 @@
-const express = require("express");
 const bcrypt = require("bcryptjs");
 const mongodb = require("mongodb");
 
-const app = express();
-
 // Requires official Node.js MongoDB Driver 3.0.0+
 
-let client = mongodb.MongoClient;
-let url = "mongodb://Fizz:Buzz123@ds151007.mlab.com:51007/heroku_blmkvj2v";
+const url = "mongodb://Fizz:Buzz123@ds151007.mlab.com:51007/heroku_blmkvj2v";
+const client = mongodb.MongoClient(url, { useNewUrlParser: true });
+const closeConnection = client.close();
+const connection = client.connect();
+const connect = connection;
+let dbInfo;
+
+connect.then(() => {
+  console.log("connected to db");
+  let db = client.db("heroku_blmkvj2v");
+
+  let collection = db.collection("users");
+  let query = {};
+
+  let cursor = collection.find(query);
+  cursor.forEach(function(doc) {
+    dbInfo = doc;
+  });
+});
 
 function fizzBuzzLogin(req, res) {
-  client.connect(url, function(err, client) {
-    let db = client.db("heroku_blmkvj2v");
-
-    let collection = db.collection("users");
-
-    collection.find("Fizz");
-
-    let query = {};
-
-    let cursor = collection.find(query);
-
-    cursor.forEach(
-      function(doc) {
-        if (
-          req.body.user === doc.user &&
-          bcrypt.compareSync(req.body.password, doc.password) === true
-        ) {
-          res.status(200).send({ response: "Access Granted" });
-        } else {
-          res.sendStatus(401);
-        }
-      },
-      function(err) {
-        client.close();
-      }
-    );
-
-    // Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
-  });
+  if (
+    req.body.user === dbInfo.user &&
+    bcrypt.compareSync(req.body.password, dbInfo.password) === true
+  ) {
+    res.status(200).send({ response: "Access Granted" });
+    closeConnection;
+    console.log("Auth Success - Connection Closed");
+  } else {
+    res.sendStatus(401);
+  }
 }
 
 module.exports.fizzBuzzLogin = fizzBuzzLogin;
