033645307866a441ffd6c245d262104a483c420e
EricV-Dev <eric_j_villarreal@Yahoo.com>
2019-08-29 15:21:32 -0500

    .

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.get("/", (req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Origin: *");
+  res.header("Access-Control-Allow-Headers: *");
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    res.header("Access-Control-Allow-Methods : PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
   }
   next();
